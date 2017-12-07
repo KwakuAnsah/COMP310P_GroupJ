d7174cf39905912b86c489109b46d3b662e4fe1c
Danielle.Luxton <Danielle.Luxton@MSI.home>
2017-12-07 23:42:28 +0000

    Lastest commitdedsdfsf

---
+++ public/header.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lanh="eng">
+    <head>
+        <title> MovieTime Homepage</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device width, intial-scale=1">
+         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    </head>
+    
+    <body>
+          <div class="container text-center "> 
+        
+            <h1 style="display: inline; font-size:500% " >MovieTime     </h1>
+            
+<!--            <img src="new_logo.jpg" alt="image of logo" height="150px" style="display: inline;">-->
+         </div>
+        
+        <div class="container-fluid">
+            <nav class="navbar navbar-inverse">
+                
+                <div class="container-fluid">
+<!--                  <div class="navbar-header">
+                    <a class="navbar-brand" href="#">WebSiteName</a>
+                  </div>-->
+                  <ul class="nav navbar-nav ">
+                    <li class="active"><a href="#">Home</a></li>
+                    <li><a href="#">What's on</a></li>
+                    <li><a href="#">Locations</a></li> 
+                    <li><a href="#">Film Genres </a></li>
+                     <li><a href="#">Page 1</a></li>
+                    <li><a href="#">Page 2</a></li>
+                    <li><a href="#">Page 3</a></li>
+                    
+                    
+                    
+                  </ul>
+                </div>
+            </nav>
+        </div>  
+        
+        
+         <div class="container"> 
+             <div class="row">
+                 <div class="col-md-2" style='background-color: #ff9999' style="height: 30%; "> What's on </div>
+                 <div class="col-md-2" style='background-color: #ff2999'> Locations </div>
+                 <div class="col-md-2" style='background-color: #ff9999'> Film Genres </div>
+                 <div class="col-md-2" style='background-color: #99ccff'> About Us </div>
+                 <div class="col-md-2" style='background-color: #00cc99'> Contact Us </div>
+                 <div class="col-md-2" style='background-color: #ff9999'> Search </div>
+              </div>
+              
+         </div>
+             
+       
+    </body>
+    
+</html>
