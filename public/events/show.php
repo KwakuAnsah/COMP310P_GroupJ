5c9468c067df9645fb7e0443fe09899f0479f2d7
jennyJL <>
2017-12-10 19:55:46 +0000

    Event show page - just needs small tweaks to show the names rather than id's - I will style it after it's fully working

--- public/events/show.php
+++ public/events/show.php
@@ -1,50 +1,84 @@
-<?php
+<!DOCTYPE html>
+<html lang="eng">
 
-require_once('../../private/initialize.php');
+    <head>
+        <?php
+        require_once('../../private/initialize.php');
 // $id = isset($_GET['id']) ? $_GET['id'] : '1';
-$id = $_GET['id'] ?? '1'; // PHP > 7.0
-$event = find_event_by_id($id);
+        $id = $_GET['event_id'] ?? '1'; // PHP > 7.0
+        $event = find_event_by_id($id);
 // this enables us to display the event info
 // further down the webevent
-?>
+        ?>
+        <?php $page_title = 'Show Events Page'; ?>
+    </head>
 
-<?php $page_title = 'Show Page'; ?>
-<?php include(SHARED_PATH . '/header.php'); ?>
-
-<div id="content">
-
-    <a class="back-link" href="<?php echo url_for('/events/host_pages/index.php');
-?>">&laquo; Back to List</a>
-
-    <div class="event show">
-
-        <h1>Page: <?php echo h($event['menu_name']); ?></h1>
-
-        <div class="attributes">
-            <?php $subject = find_subject_by_id($event['subject_id']); ?>
-            <dl>
-                <dt>Subject</dt>
-                <dd><?php echo h($subject['menu_name']); ?></dd>
-            </dl>
-            <dl>
-                <dt>Menu Name</dt>
-                <dd><?php echo h($event['menu_name']); ?></dd>
-            </dl>
-            <dl>
-                <dt>Position</dt>
-                <dd><?php echo h($event['position']); ?></dd>
-            </dl>
-            <dl>
-                <dt>Visible</dt>
-                <dd><?php echo $event['visible'] == '1' ? 'true' : 'false'; ?></dd>
-            </dl>
-            <dl>
-                <dt>Content</dt>
-                <dd><?php echo h($event['content']); ?></dd>
-            </dl>
+    <body>
+    <!-- * * * * * ADD CORRECT HEADER FILE? * * * * *-->
+        <?php include(SHARED_PATH . '/header.php'); ?>
+        <div class="container">
+            <div id="content">
+                <a class="back-link" href="<?php echo url_for('/events/host_pages/index.php'); ?>">&laquo; Back to List</a>
+                <div class="event show">
+                    <h1><?php echo h($event['event_name']); ?></h1>
+                    <div class="attributes"> 
+                        <h2>Event Details</h2>
+                        <dl>
+                            <dt>Event type:</dt>
+                            <dd><?php echo h($event['event_category_id']); ?></dd>
+                        </dl>
+                        <dl>
+                            <dt>Hosted by:</dt>
+                            <dd><?php echo h($event['host_user_id']); ?></dd>
+                        </dl>
+                        <dl>
+                            <dt>Host rating:</dt>
+    <!-- * * * * * * * * * * INSERT host rating * * * * * * * * * * -->
+                            <dd><?php echo h($XXX['XXX']); ?></dd>
+                        </dl>
+                        <!-- Not sure what this is for?
+                        <dl>
+                            <dt>Visible:</dt>
+                            <dd><?php echo $event['visible'] == '1' ? 'true' : 'false'; ?></dd>
+                        </dl>
+                        -->
+                        <dl>
+                            <dt>Event Description:</dt>
+                            <dd><?php echo h($event['event_description']); ?></dd>
+                        </dl>
+                        <dl>
+                            <dt>Films showing:</dt>
+    <!-- * * * * * * * * * * INSERT films to be shown * * * * * * * * * * -->
+                            <dd><?php echo h($XXX['XXX']); ?></dd>
+                        </dl>
+                        <br>
+                        <h2>Date and Time</h2>
+                        <dl>
+                            <dt>Start:</dt>
+                            <dd><?php echo h($event['event_start']); ?></dd>
+                        </dl>
+                        <dl>
+                            <dt>End:</dt>
+                            <dd><?php echo h($event['event_end']); ?></dd>
+                        </dl>
+                        <br>
+                        <h2>Location</h2>
+                        <dl>
+                            <dt>Room:</dt>
+                            <dd><?php echo h($event['room_id']); ?></dd>
+                        </dl>
+    <!-- * * * * * Include rest of address? - address id, postcode, city id, country id etc.? * * * * * -->       
+                        <br>
+                        <h2>More Information</h2>
+                        <dl>
+                            <dt>Room is wheelchair accessible:</dt>
+    <!-- * * * * * * * * * * Include this here? Or show this on the locations page? * * * * * * * * * * -->                        
+                            <dd><?php echo h($XXX['XXX']); ?></dd>
+                        </dl>
+                    </div>                  
+                </div>
+            </div>
         </div>
-
-    </div>
-
-</div>
-<?php include(SHARED_PATH . '/footer.php'); ?>
+    </body>
+    
+    <?php include(SHARED_PATH . '/footer.php'); ?>
