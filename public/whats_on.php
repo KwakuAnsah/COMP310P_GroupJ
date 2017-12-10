431e4e54314a9f08a663934c52960cead506310a
kwaku <kwaku@Kwaku-PC>
2017-12-10 16:30:03 +0000

    Whats up page

---
+++ public/whats_on.php
@@ -0,0 +1,39 @@
+
+<!DOCTYPE html>
+
+<html lang="eng">
+    <head>
+         <?php require_once('../private/initialize.php'); ?>    
+        <?php $page_title = 'MovieTime - Home Page'; ?>
+
+            <title> Whats On</title>
+
+        <?php include "bootstrap.php";?>
+        <!--    CSS to style sidenav-->
+        <link rel="stylesheet for sidenav" href="whats_on CSS.css">  
+    
+    </head>
+    
+    <body> 
+       
+         <?php include "header.php";?>     
+        
+        <div class="container-fluid text-center">
+            <div class="row content">
+                <div class="col-sm-3 sidenav">
+                   <p><img src="avengers poster.jpeg" class="img-thumbnail img" alt=Avengers_Poster"> Watch the Lastest Avengers movie with your friends NOW</p>
+                   <p><img src="got poster.jpg" class="img-thumbnail img" alt="GOT_Poster"> Watch the Lastest GOT series with your friends NOW</p>
+                   <p><img src="Justice league poster.jpg" class="img-thumbnail img" alt="Justice League Poster"> Watch the Lastest Justice League movie with your friends NOW</p>
+                </div>
+            </div>  
+        </div>
+      
+        
+             
+        
+    </body>
+    
+        
+         
+</html>    
+
