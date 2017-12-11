d1fd4e6fd956affeed3f19954c32752df5f674f7
Danielle.Luxton <Danielle.Luxton@MSI.home>
2017-12-11 16:59:12 +0000

    Confirmation page - Fully complete

--- public/bookings/confirmation.php
+++ public/bookings/confirmation.php
@@ -9,17 +9,22 @@
 
         <!-- CSS stylesheet for Bookings pages-->
         <link href="../stylesheets/bookings_style.css" rel="Bookings stylesheet">
-        
+
+        <?php include(SHARED_PATH . '/header.php'); ?>
+
         <?php
         $id = $_GET['booking_id'] ?? '1';
         $booking = find_booking_by_id($id);
-        
+
+        $user_id = $_GET['user_id'] ?? '1'; // PHP > 7.0
+        $user = find_user_by_id($user_id);
         ?>
+
+
+
     </head>
-    
+
     <body>
-    <!-- * * * * * UPDATE HEADER FILE * * * * * -->    
-        <?php include "../../public/header.php"; ?>
         <div class="container">
             <h1 class="heading-centre">Booking Confirmation</h1>
             <div class="col-md-1"></div>
@@ -29,16 +34,14 @@
                     <br>
                     <br>
                     <p class="message">
-                        Your booking ID: <i><!-- * * * * * INSERT USER'S BOOKING ID * * * * * -->  <?php echo h($booking_has_user['booking_id']); ?>
-                        </i>
+                        <b>Your booking ID:</b>
+                        <br>
+                        <i><?php echo h($booking['booking_id']); ?></i>
                         <br>
                         <br>
-                        A confirmation email has been sent to:
+                        <b>A confirmation email has been sent to:</b>
                         <br>
-                        <i>
-    <!-- * * * * * INSERT USER'S EMAIL ADDRESS * * * * * --> 
-                        <?php echo h($user['email']); ?>
-                        </i>
+                        <i><?php echo h($user['email']); ?></i>
                         <br>
                         <br>
                         <b>Please bring your confirmation email with you to the event, otherwise entry will not be permitted.</b>
@@ -47,4 +50,25 @@
             </div>  
             <div class="col-md-1"></div>
         </div>
+        <br>
+        <br>
+        <br>
+        <div class="container">
+            <div class="col-md-2"></div>
+            <div class="col-md-3">
+                <a href="../../public/users/show.php" class="view btn btn-lg btn-default">View my Account</a>
+            </div>
+            <div class="col-md-1"></div>
+            <div class="col-md-3">
+                <a href="../../public/index.php" class="view btn btn-lg btn-default">Continue Browsing</a>
+            </div>
+            <div class="col-md-2"></div>
+        </div>
+        <br>
+        <br>
+        <br>
+
+
+
+
     </body>
